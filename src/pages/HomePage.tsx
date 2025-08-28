9804ab3af0f457e54d250aeb747f474606736f04
Djonahuti <djonah04@gmail.com>
2025-08-28 10:14:23 +0100

    commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -150,7 +150,7 @@ export default function HomePage() {
       </div>
 
       {/* CTA Section */}
-      <div className="bg-gradient-to-r from-gray-600 to-primary-light">
+      <div className="bg-gradient-to-r from-gray-600 to-primary-light shimmer-effect">
         <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
           <div className="mx-auto max-w-2xl text-center">
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
