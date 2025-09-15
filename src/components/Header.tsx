d568c92a1640b60147480762ab9c26aae9f9de0e
Djonahuti <djonah04@gmail.com>
2025-09-15 13:34:27 +0100

    commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -128,7 +128,7 @@ export default function Header() {
                       className={`-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 ${
                         location.pathname === item.href
                           ? 'text-primary dark:text-primary-light bg-gray-50'
-                          : 'text-gray-900 dark:text-gray-300 hover:bg-gray-50'
+                          : 'text-gray-900 dark:text-gray-300 hover:text-primary dark:hover:text-primary-light transition-colors'
                       }`}
                       onClick={() => setMobileMenuOpen(false)}
                     >
