dba46ef6a9475308d4543404fe53eb9f5e442ac9
Djonahuti <djonah04@gmail.com>
2025-09-15 07:29:14 +0100

    commit

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,17 @@
+import { IconMoonFilled, IconSunFilled } from "@tabler/icons-react";
+import { useTheme } from "./ThemeProvider";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <button
+      className="p-2 rounded-lg transition"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      {theme === "light" ? <IconMoonFilled size={20} className="text-blue-800" /> : <IconSunFilled size={20} className="text-[#FFDF00]" />}
+    </button>
+  );
+};
+
+export default ThemeToggle;
